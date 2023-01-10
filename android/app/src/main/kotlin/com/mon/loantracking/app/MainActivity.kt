f6b10bf26a93d8c06da962fb166f957ba13a0675
David Andrew Francis Duldulao <dafduldulao@gmail.com>
2023-01-10 23:43:35 +0800

    initial commit

---
+++ android/app/src/main/kotlin/com/mon/loantracking/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mon.loantracking.app.flutter_mon_loan_tracking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_mon_loan_tracking/app/app.dart';
+import 'package:flutter_mon_loan_tracking/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
