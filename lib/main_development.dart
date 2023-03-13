d71eee3e3e85e04d896dc25d92217df4010b567a
David Andrew Francis Duldulao <dafduldulao@gmail.com>
2023-03-13 11:30:06 +0800

    fixed mobile screen

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mon_loan_tracking/app/app.dart';
 import 'package:flutter_mon_loan_tracking/bootstrap.dart';
@@ -6,10 +7,14 @@ import 'package:flutter_mon_loan_tracking/bootstrap.dart';
 import 'package:flutter_mon_loan_tracking/firebase_options_dev.dart';
 import 'package:flutter_mon_loan_tracking/services/environments.dart';
 import 'package:flutter_mon_loan_tracking/utils/constants.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:flutter_mon_loan_tracking/utils/platform_stub.dart'
+  if (kIsWeb)
+    'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 Future<void> main() async {
-  usePathUrlStrategy();
+  if (kIsWeb) {
+    usePathUrlStrategy();
+  }
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
---
+++ lib/utils/platform_stub.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_mon_loan_tracking/utils/print_utils.dart';
+
+void usePathUrlStrategy() {
+  printd('[STUB] usePathUrlStrategy');
+}
